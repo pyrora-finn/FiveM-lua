3c28fc8b1a25ef4727a18f5a69a1bf2208f18422
Finn <153606679+pyrora-finn@users.noreply.github.com>
2024-10-26 21:58:37 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dein Name'
+description 'Ein einfaches Beispiel für ein FiveM-Skript'
+version '1.0.0'
+
+server_script 'server.lua'
+client_script 'client.lua'
